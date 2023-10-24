3bfa5d16bd86996d960c860ac6017d7108a03732
Ocepow <ocepow@outlook.fr>
2023-10-24 18:25:15 +0200

    excuse finish

--- rules/src/rules/PlayCardRule.ts
+++ rules/src/rules/PlayCardRule.ts
@@ -158,7 +158,7 @@ export class PlayCardRule extends PlayerTurnRule {
 
         const excuseOnTable = this.material(MaterialType.Card).location(LocationType.Table).id(Card.Excuse);
 
-        if (excuseOnTable.length === 1 && !this.isSameSide(trickWinner, excuseOnTable.getItem()!.location.player!)) {
+        if (excuseOnTable.length === 1 && !this.isSameSide(trickWinner, excuseOnTable.getItem()!.location.player!) && !this.isLastTrick) {
           moves.push(
             excuseOnTable.moveItem({ location: { type: LocationType.Tricks, player: excuseOnTable.getItem()?.location.player }, rotation: { y: 0 } })
           )
