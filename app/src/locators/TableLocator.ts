0a40065d997146a5dd45ea21f648537397eaa9f4
Ocepow <ocepow@outlook.fr>
2023-10-30 11:38:36 +0100

    table locator

--- app/src/locators/TableLocator.ts
+++ app/src/locators/TableLocator.ts
@@ -8,29 +8,22 @@ export class TarotTableLocator extends ItemLocator {
   getPosition(item: MaterialItem, context: ItemContext) {
     const relativePlayerIndex = this.getRelativePlayerIndex(context, item.location.player!)
 
-    switch (relativePlayerIndex) {
-      case 1:
-        return { x: -5, y: 0, z: item.location.z!*0.1 }
-      case 2:
-        return { x: 0, y: -5, z: item.location.z!*0.1 }
-      case 3:
-        return { x: 5, y: 0, z: item.location.z!*0.1 }
-    }
-    return { x: 0, y: 5, z: item.location.z!*0.1 }
+    const players = context.rules.players.length
+    const angle = 270 - relativePlayerIndex * 360 / players
+    const radiusX = 5
+    const radiusY = 5
+    const x = Math.cos(angle * Math.PI / 180) * radiusX
+    const y = -Math.sin(angle * Math.PI / 180) * radiusY
+    return { x, y, z: item.location.z! * 0.1 }
   }
+
+
+
 
   getRotation(item: MaterialItem, context: ItemContext): number {
     const relativePlayerIndex = this.getRelativePlayerIndex(context, item.location.player!)
-    switch (relativePlayerIndex) {
-      case 1:
-        return 90
-      case 2:
-        return 180
-      case 3:
-        return -90
-      default:
-        return 0
-    }
+    const players = context.rules.players.length
+    return relativePlayerIndex * 360 / players
   }
 }
 
