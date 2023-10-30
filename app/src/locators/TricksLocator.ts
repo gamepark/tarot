c3105346e117d1c09fdcdbb7843b6064d83cf800
Ocepow <ocepow@outlook.fr>
2023-10-30 11:30:22 +0100

    trick locator

--- app/src/locators/TricksLocator.ts
+++ app/src/locators/TricksLocator.ts
@@ -6,29 +6,33 @@ export class TricksLocator extends DeckLocator {
   locationDescription = new TarotDeckLocatorDescription()
   delta = { x: -0.05, y: -0.05, z: 0.1 }
 
-  
+
   isHidden(item: MaterialItem) {
-    return item.rotation?.y===1;
-   }
+    return item.rotation?.y === 1;
+  }
 
   getCoordinates(item: MaterialItem<number, number>, context: ItemContext) {
     const relativePlayerIndex = this.getRelativePlayerIndex(context, item.location.player!)
-    switch (relativePlayerIndex) {
-      case 1:
-        return { x: -44, y: 21, z: 0 }
-      case 2:
-        return { x: -25, y: -25, z: 0 }
-      case 3:
-        return { x: 44, y: -21, z: 0 }
-    }
-    return { x: 25, y: 25, z: 0 }
+    const players = context.rules.players.length
+    const angle = 270 - relativePlayerIndex * 360 / players
+    const radiusX = players === 5 ? 25 : players === 4 ? 25 : 25
+    const radiusY = players === 5 ? 20 : players === 4 ? 20 : 15
+
+    const x = Math.cos(angle * Math.PI / 180) * radiusX
+    const y = -Math.sin(angle * Math.PI / 180) * radiusY
+
+    return { x, y, z: 10 }
   }
 
   getRotation(item: MaterialItem<number, number>, context: ItemContext<number, number, number>): number {
     const relativePlayerIndex = this.getRelativePlayerIndex(context, item.location.player!)
-    return relativePlayerIndex % 2 === (item.rotation?.y?? 0) ? 0 : 90 
+    const players = context.rules.players.length
+
+    return relativePlayerIndex * 360 / players + 90
+
   }
 }
+
 
 
 class TarotDeckLocatorDescription extends LocationDescription {
