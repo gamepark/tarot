68083555e0b8faa3d4c9d34b28caaa1486418915
Ocepow <ocepow@outlook.fr>
2023-11-08 21:12:54 +0100

    agrandir pannel

--- app/src/panels/PlayerPanels.tsx
+++ app/src/panels/PlayerPanels.tsx
@@ -36,8 +36,8 @@ const panelPosition = (index: number) => css`
   position: absolute;
   right: 1em;
   top: ${8.5 + index * 16}em;
-  width: 28em;
-  height: 14em;
+  width: 35em;
+  height: 15em;
 `
 
 const bidCss = css`
