c879a93478f605a28f1a1e7ba741fa6c75c8b134
Ocepow <ocepow@outlook.fr>
2023-11-20 15:11:31 +0100

    number of rounds

--- app/src/GameDisplay.tsx
+++ app/src/GameDisplay.tsx
@@ -1,9 +1,12 @@
 /** @jsxImportSource @emotion/react */
-import { GameTable } from '@gamepark/react-game'
+import { GameTable, useRules } from '@gamepark/react-game'
 import { PlayerPanels } from './panels/PlayerPanels'
 import { pointerWithin } from '@dnd-kit/core'
 import { ScoringDialog } from './headers/ScoringDialog'
 import { css } from '@emotion/react'
+import { TarotRules } from '@gamepark/tarot/TarotRules'
+import { useTranslation } from 'react-i18next'
+import { Memory } from '@gamepark/tarot/rules/Memory'
 
 export default function GameDisplay() {
   return <>
@@ -11,9 +14,31 @@ export default function GameDisplay() {
       margin={{ top: 7, left: 0, right: 30, bottom: 0 }} collisionAlgorithm={pointerWithin}
       css={css`border: 1px solid white`} />
 
+
+    <RoundNumber />
+
     <PlayerPanels />
 
     <ScoringDialog />
 
   </>
 }
+
+
+const RoundNumber = () => {
+  const rules = useRules<TarotRules>()
+  const { t } = useTranslation()
+  if (!rules) {
+    return null
+  }
+  return <div css={roundCss}><span>{t('round', { round: rules.remind(Memory.Round), total: 4 })}</span></div>
+}
+
+const roundCss = css`
+position:absolute;
+right:3em;
+bottom:2em;
+> span {
+  font-size: 4em;
+}
+`
