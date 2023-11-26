a292c9e6f0d03f9943ae832fa1e979ffabd1db88
Ocepow <ocepow@outlook.fr>
2023-11-26 20:54:13 +0100

    cardrules id ndefined

--- app/src/material/CardRule.tsx
+++ app/src/material/CardRule.tsx
@@ -8,6 +8,8 @@ import { LocationType } from "@gamepark/tarot/material/LocationType"
 import { MaterialType } from "@gamepark/tarot/material/MaterialType"
 import { useTranslation } from "react-i18next"
 
+
+// ICI appelle la TarotCardHelp (on a changé le type)
 export const TarotCardRule = (props: MaterialRulesProps) => {
   const rules = useRules<TarotRules>()!
   const { t } = useTranslation()
@@ -18,25 +20,20 @@ export const TarotCardRule = (props: MaterialRulesProps) => {
   const player = usePlayerId<Number>()
   const playerName = usePlayerName(item.location!.player!)
 
-
-
-
-
   return <>
-    {item.id !== undefined &&
-      <section css={cardText}>
-        <h3>{t(`cardrules.${item.id}`)}</h3>
+    <section css={cardText}>
+        <h3>{t(item.id !== undefined ?`cardrules.${item.id}`: "cardrule.???")}</h3>
         {deck && <p>{t('rules.card.deck', { number: rules.material(MaterialType.Card).location(LocationType.Deck).length })}</p>}
         {hand && item.location?.player === player && <p>{t('rules.card.hand')}</p>}
-        {hand && item.location?.player !== player && <p>{t('rules.card.hand.other' , {player: playerName})} </p>}
-
+        {hand && item.location?.player !== player && <p>{t('rules.card.hand.other', { player: playerName })} </p>}
+        
         {kitty && <p>{t('rules.card.kitty')}</p>}
         {item.id === Card.Excuse && <p>{t('rules.card.excuse')}</p>}
         {item.id === Card.Excuse && <p>{t('rules.card.oudlers')}</p> || item.id === Card.Trump1 && <p>{t('rules.card.oudlers')}</p> || item.id === Card.Trump21 && <p>{t('rules.card.oudlers')}</p>}
 
 
       </section>
-    }
+    
   </>
 }
 
