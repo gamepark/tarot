9d8431c36a54744274007578aa9b4e52aaf4040d
Ocepow <ocepow@outlook.fr>
2023-11-28 11:46:27 +0100

    text card

--- app/src/material/CardRule.tsx
+++ app/src/material/CardRule.tsx
@@ -14,15 +14,9 @@ import { FC } from "react"
 export const TarotCardHelp = (props: MaterialHelpProps) => {
   const { t } = useTranslation()
   const { item } = props
-  const kitty = item.location?.type === LocationType.Kitty
   const oudlers = isOudler(item.id)
   const trump = isTrump(item.id)
 
-
-  { kitty && <p>{t('card.kitty')}</p> }
-  { kitty && <p>{t('rules.card.kitty.small.guard')}</p> }
-  { kitty && <p>{t('rules.card.kitty.guard.without')}</p> }
-  { kitty && <p>{t('rules.card.kitty.guard.against')}</p> }
 
 
 
@@ -33,25 +27,25 @@ export const TarotCardHelp = (props: MaterialHelpProps) => {
       <h3 css={titre}>{t(item.id !== undefined ? `cardrules.${item.id}` : "cardrule.other")}</h3>
       <LocationHelp {...props} />
 
-
+      {trump && <p>{t('rules.trump')}</p>}
 
       {oudlers && <p>{t('rules.card.oudlers')}</p>}
+      {oudlers && <p>{t('rules.contrat')}</p>}
 
-      {trump && <p>{t('rules.trump')}</p>}
+
       {item.id === Card.Excuse && <p>{t('rules.card.excuse')}</p>}
       {item.id === Card.Trump1 && <p>{t('rules.card.petit')}</p>}
       {item.id === Card.Trump1 && <p>{t('rules.card.petit.bout.excuse.chelem')}</p>}
 
-      { item.id !== undefined && (
+
+
+
+      {item.id !== undefined && (
         <>
           <hr />
-          <p>{t(`cardpoints`, { value: cardValue(item.id)})}</p>
+          <p>{t(`cardpoints`, { value: cardValue(item.id) })}</p>
         </>
-      ) }
-      {oudlers && <p>{t('rules.contrat')}</p>}
-
-
-
+      )}
 
     </section>
 
@@ -76,27 +70,44 @@ export const LocationHelp: FC<MaterialHelpProps> = (props) => {
   const table = item.location?.type === LocationType.Table
   const trick = item.location?.type === LocationType.Tricks
   const ecart = item.location?.type === LocationType.Ecart
+  const kitty = item.location?.type === LocationType.Kitty
 
-  if (!deck && !hand && !table && !trick && !ecart) return null;
+  if (!deck && !hand && !table && !trick && !ecart && !kitty) return null;
 
   return (
     <>
       {ecart && (
         <>
           <p>{t('card.ecart')}</p>
-          <hr/>
+          <hr />
           <p>{t('rules.card.ecart')}</p>
         </>
       )}
 
       {deck && <p>{t('rules.card.deck', { number: rules.material(MaterialType.Card).location(LocationType.Deck).length })}</p>}
+
+
       {hand && item.location?.player === player && <p>{t('rules.card.hand')}</p>}
       {hand && item.location?.player !== player && <p>{t('rules.card.hand.other', { player: playerName })} </p>}
 
       {trick && item.location?.player === player && <p>{t('rules.card.trick')}</p>}
       {trick && item.location?.player !== player && <p>{t('rules.card.trick.other', { player: playerName })} </p>}
+
       {table && <p>{t('rules.card.table')}</p>}
+
+      {kitty && (
+        <>
+          <p>{t('card.kitty')}</p>
+          <hr />
+          <p>{t('rules.card.kitty.small.guard')}</p>
+          <p>{t('rules.card.kitty.guard.without')}</p>
+          <p>{t('rules.card.kitty.guard.against')}</p>
+        </>
+      )}
+
+
     </>
+
 
 
   )
@@ -109,7 +120,5 @@ const cardText = css`
   }
 `
 const titre = css`
-  h3 {
-    margin: 0.3em 0;
-  }
+    text-align: center;
 `
