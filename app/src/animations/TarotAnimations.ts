e7ef7b1c820caacb5a2637bfeb062f3f485e40cb
Romain Fromi <romain.fromi@gmail.com>
2024-01-06 15:22:22 +0100

    Faster animation when cards goes to tricks

--- app/src/animations/TarotAnimations.ts
+++ app/src/animations/TarotAnimations.ts
@@ -1,7 +1,11 @@
 import { MaterialGameAnimations } from '@gamepark/react-game'
 import { isMoveItem } from '@gamepark/rules-api'
+import { LocationType } from '@gamepark/tarot/material/LocationType'
 import { RuleId } from '@gamepark/tarot/rules/RuleId'
 
 export const tarotAnimations = new MaterialGameAnimations()
 
 tarotAnimations.when().rule(RuleId.Deal).move(isMoveItem).duration(0.2)
+tarotAnimations.when()
+  .move(move => isMoveItem(move) && move.location.type === LocationType.Tricks)
+  .duration(0.3)
