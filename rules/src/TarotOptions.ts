146ac401ad2d3a053078beecd2dfc83e28f422ab
Patrick Beaume <p.beaume@gmail.com>
2024-05-22 08:41:37 +0200

    add options for number of round in friendly mode

--- rules/src/TarotOptions.ts
+++ rules/src/TarotOptions.ts
@@ -5,11 +5,20 @@ import { OptionsSpec } from '@gamepark/rules-api'
  * The first generic parameter, "{}", can be changed to include game options like variants or expansions.
  */
 export type TarotOptions = {
-  players: number
+  players: number,
+  round: number
 }
 
 /**
  * This object describes all the options a game can have, and will be used by GamePark website to create automatically forms for you game
  * (forms for friendly games, or forms for matchmaking preferences, for instance).
  */
-export const TarotOptionsSpec: OptionsSpec<TarotOptions> = {}
+export const TarotOptionsSpec: OptionsSpec<TarotOptions> = {
+  round: {
+    label: (t) => t('round.count'),
+    help: (t) => t('round.count.help'),
+    competitiveDisabled: true,
+    values: [1, 2, 3, 4, 5],
+    valueSpec: r => ({ label: () => `${r}` })
+  }
+}
